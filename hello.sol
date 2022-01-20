21f5c99b95b3ee17a65533b44e64cedc6af5cf10
parag jagdale <paragj2801@gmail.com>
2022-01-21 00:43:02 +0530

    helloWorld

--- hello.sol
+++ hello.sol
@@ -5,3 +5,14 @@ contract HelloWorld{
         return "Hello Wrold";
     }
 }
+
+// Types and Variables
+
+// Js is untayped Language 
+// solidty 
+// int a = 1;  // singed integer (both +ve and -ve number)
+// uint aa = 2 // unsigned integer (only positive only nr >= 0)
+// string b = "hello"
+// bool c = false
+// a = "hello" "Not work coz we declare a as int  throw the error
+// address  d = ethereum address
